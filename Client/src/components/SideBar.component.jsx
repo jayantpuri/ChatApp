ec7898b3507049db2a4b4eda65d8541df389538d
jayant puri <jayantpuri16@gmail.com>
2024-01-16 19:32:19 -0800

    edited sidebar component

--- Client/src/components/SideBar.component.jsx
+++ Client/src/components/SideBar.component.jsx
@@ -1,17 +1,7 @@
 import React from "react";
-import {
-  Drawer,
-  DrawerBody,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerOverlay,
-  DrawerContent,
-  DrawerCloseButton,
-  useDisclosure,
-} from "@chakra-ui/react";
 
 const SideBar = ({ children }) => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  
   return <div>{children}</div>;
 };
 
