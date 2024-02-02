f3a110b284c78eba54de959a7ef173e9e0a1cc27
jayant puri <jayantpuri16@gmail.com>
2024-02-01 20:47:00 -0800

    final commit

--- Server/expressServer.js
+++ Server/expressServer.js
@@ -10,29 +10,34 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-app.get("/", (req, res) => {
-  res.json("Server is running");
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
 });
+
 app.use(express.json());
 app.use("/api/user", userRouter);
 app.use("/api/chat", chatRouter);
 app.use("/api/message", messageRouter);
 
-
 // ----------------Deployment-------------------------
-// const dir = path.resolve();
-// if (process.env.NODE_ENV === "production") {
-//   console.log('yes')
-//   app.use(express.static(path.join(dir, "client/build")));
+const dir = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  console.log('yes')
+  app.use(express.static(path.join(dir, "Client/build")));
 
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(dir, "client/build", "index.html"));
-//   });
-// } else {
-//   console.log("nope");
-// }
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(dir, "Client", "build", "index.html"));
+  });
+} else {
+  console.log("nope");
+}
 
 // ----------------Deployment-------------------------
 
